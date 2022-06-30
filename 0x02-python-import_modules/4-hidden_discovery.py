8d96f059751b52678e41227f6f9d5bf23c8e7826
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-06-30 20:46:11 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import hidden_4
-
-    names = dir(hidden_4)
-
-    for name in names:
-        if "__" not in name[:2]:
-            print("{:s}".format(name))
+    from hidden_4 import *
+    for d in dir():
+        if d[0:2] != "__":
+            print("{}".format(d))
