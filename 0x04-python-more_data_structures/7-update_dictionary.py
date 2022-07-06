e9ff5a0e168e71c32f9b923a7a0901e4bf09319d
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-07-06 23:04:11 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def update_dictionary(my_dict, key, value):
-    my_dict[key] = value
+def update_dictionary(a_dictionary, key, value):
+    '''INPUT:
+         a_dictionary: Input dictionary
+         key: argument will be always a string
+         value: argument will be any type
+       RETURN: a dictionary
+    A function that replaces or adds key/value in a dictionary
+    '''
+    a_dictionary[key] = value
     return 
