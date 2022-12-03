753266f9b947c21fae96fd139aebb4e1cc01aa8c
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-12-03 11:01:09 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the size of the body response
-curl -s "$1" | wc -c
+# Displays the size of the body of the response of a curl request
+curl -so /dev/null -w '%{size_download}\n' "$1"
